bbcd61f6791f387a5a9c018025213465128155c2
Elena Note <elebeatrizgonzalez@gmail.com>
2020-11-29 15:22:49 -0300

    Updates

---
+++ precache-manifest.fa88ff86b71314076bcb6090fd626115.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf2b67923b4b45fde0cbcff3769a2a25",
+    "url": "/buscadorgifs/index.html"
+  },
+  {
+    "revision": "e7e82b3f6b668f46be69",
+    "url": "/buscadorgifs/static/css/main.0ee850bc.chunk.css"
+  },
+  {
+    "revision": "8a60d01f4879c6590d0d",
+    "url": "/buscadorgifs/static/js/2.ba081d2b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/buscadorgifs/static/js/2.ba081d2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7e82b3f6b668f46be69",
+    "url": "/buscadorgifs/static/js/main.53b55078.chunk.js"
+  },
+  {
+    "revision": "4a33b47fb11b46986259",
+    "url": "/buscadorgifs/static/js/runtime-main.4acbe8f4.js"
+  }
+]);
